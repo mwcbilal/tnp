b2da6ea648883ada218f6d0eebd0f43c7913df9b
usman-90 <siddiquiusman328@gmail.com>
2024-04-17 16:22:36 +0500

    vercel fix

--- app/api/packages/route.ts
+++ app/api/packages/route.ts
@@ -1,25 +1,26 @@
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import { NextResponse } from 'next/server';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from 'next/server';
 
-// type ResponseData = {
-// }
+type ResponseData = {
+  message: string
+}
 
-// export async function GET(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-//   try {
-//     return new NextResponse('Success', { status: 200 });
-//   } catch (error) {
-//     console.error('Error in GET handler:', error);
-//     return new NextResponse('Internal Server Error', { status: 500 });
-//   }
-// }
+export async function GET(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  try {
+    return new NextResponse('Success', { status: 200 });
+  } catch (error) {
+    console.error('Error in GET handler:', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
 
-// export async function POST(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-//   try {
-//     // Your logic here
-//     // ...
-//     return new NextResponse<ResponseData>("Success", { status: 200 });
-//   } catch (error) {
-//     console.error('Error in POST handler:', error);
-//     return new NextResponse('Internal Server Error', { status: 500 });
-//   }
-// }
+export async function POST(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  try {
+    // Your logic here
+    // ...
+    return new NextResponse<ResponseData>("Success", { status: 200 });
+  } catch (error) {
+    console.error('Error in POST handler:', error);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
